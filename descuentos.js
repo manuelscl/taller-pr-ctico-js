ad1553dd75b060028cfd773a0b8308ac49587689
Manuelcl <salvadordevlooper@gmail.com>
2022-11-15 09:03:41 -0600

    Los nuevos cupones fueron añadidos, otra versión

--- descuentos.js
+++ descuentos.js
@@ -1,31 +1,53 @@
 const inputPrice = document.querySelector('#price');
-const inputDiscount = document.querySelector('#discount');
+const inputCoupon = document.querySelector('#coupon');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('#result');
 
 btn.addEventListener('click', calculateDiscountPrice);
 
+const couponsList = [];
+couponsList.push({
+    name: 'COUPON1',
+    discount: 50
+});
+couponsList.push({
+    name: 'COUPON2',
+    discount: 30
+});
+couponsList.push({
+    name: 'COUPON3',
+    discount: 20
+});
+couponsList.push({
+    name: 'COUPON4',
+    discount: 10
+});
+
 function calculateDiscountPrice() {
     const price = Number(inputPrice.value);
-    const discount = Number(inputDiscount.value);
+    const coupon = inputCoupon.value;
 
     // !price || !discount
-    if(!price && !discount) {
+    if(!price && !coupon) {
         result.innerText = "Llena el formulario";
         return
-    } else if (!price && discount) {
-        result.innerText = "Te faltó poner el precio";
-        return;
-    } else if (price && !discount) {
-        result.innerText = "Te faltó poner el descuento";
+    }
+
+    let discount;
+
+    function isCouponInArray(couponName) {
+        return couponName.name === coupon;
+    }
+    const couponsInArray = couponsList.find(isCouponInArray);
+
+    if(couponsInArray) {
+        discount = couponsInArray.discount;    
+    } else {
+        result.innerText = "El cupón no es válido";
         return;
     }
 
-    if(discount > 100) {
-        result.innerText = "Sí, claro, un descuento del 100% :)"
-        return;
-    }
-    const percentageDiscount = price * (100 - discount) / 100;
-    result.innerText = `Nuevo precio a pagar: $${percentageDiscount}`;
+    const newPrice = price * (100 - discount) / 100;
+    result.innerText = `Nuevo precio a pagar: $${newPrice}`;
 }
 
