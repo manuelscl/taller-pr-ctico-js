b3b32731018d65873a1f78c8e9534b02bbac637e
Manuelcl <salvadordevlooper@gmail.com>
2022-11-21 13:08:45 -0600

    Arreglé el error al intentar encontrar la mitad en una lista par
    Ya podemos calcular la mediana, con listas pares e impares.

--- mediana/mediana.js
+++ mediana/mediana.js
@@ -1,7 +1,6 @@
 function esPar(lista) {
     // numero / 2
     // (numero % 2)
-    console.log(!(lista.length % 2));
     return !(lista.length % 2);
 }
 // function esImpar(lista) {
@@ -12,25 +11,33 @@ function calcularMediana(lista) {
     const listaEsPar = esPar(lista);
 
     if(listaEsPar) {
-        // ...
+        const indexMitad1ListaPar = (lista.length / 2) - 1;
+        const indexMitad2ListaPar = lista.length / 2;
+        // const mitad1ListaPar = lista[(lista.length / 2) - 1];
+        // const mitad2ListaPar = lista[lista.length / 2];
+
+        const listaMitades = [];
+        listaMitades.push(lista[indexMitad1ListaPar]);
+        listaMitades.push(lista[indexMitad2ListaPar]);
+
+        calcularPromedio(listaMitades)
     } else {
-        const indexListaImpar = Math.floor(lista.length / 2);
-        const medianaListaImpar = lista[indexListaImpar];
-        console.log(indexListaImpar);
-        console.log(medianaListaImpar);
+        const indexMitadListaImpar = Math.floor(lista.length / 2);
+        const medianaListaImpar = lista[indexMitadListaImpar];
+        // console.log(indexMitadListaImpar);
+        // console.log(medianaListaImpar);
         return medianaListaImpar;
     }
 }
 
 function calcularPromedio(lista) {
-    //sumar todos los elementos del array / cantidad de elementos, lista.length.
-
-    function sumarElementos(valorAcumulado, valorNuevo) {
+    function sumarTodosElementos(valorAcumulado, valorNuevo) {
         return valorAcumulado + valorNuevo;
     }
-    // const sumarElementos = (valorAcumulado, valorNuevo) => valorAcumulado + valorNuevo;
-    const sumaLista = lista.reduce(sumarElementos);
+    const sumaLista = lista.reduce(sumarTodosElementos);
     const promedio = sumaLista / lista.length;
+
+    // const result = Array.isArray(lista)
 
     console.log(promedio);
     return promedio;
