dae5829a2d05d976382196890f8d8fd18b880c06
manuel_scl <salvadordevlooper@gmail.com>
2022-11-29 10:54:50 -0600

    Calculando cuál será tu salario el próximo año

--- analisis.js
+++ analisis.js
@@ -20,3 +20,27 @@ function medianaPorPersona(nombrePersona) {
     const medianaSalarios = PlatziMath.calcularMediana(salarios);
     console.log(medianaSalarios);
 }
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+    
+    for(let i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos[i - 1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = crecimiento / salarioPasado;
+        porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+    console.log({porcentajesCrecimiento, medianaPorcentajesCrecimiento});
+
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+
+    return nuevoSalario;
+}
